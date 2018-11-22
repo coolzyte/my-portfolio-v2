fe64524a7c21785339f24cebf875964c248e7d1f
coolzyte <coolzyte@gmail.com>
2018-11-22 12:08:20 +0100

    Updates

---
+++ precache-manifest.e9396edd3122c42243742acbf9280aef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f33e97bfaf6034b30bf6716908c428ab",
+    "url": "/my-portfolio-v2/static/media/c-logo.f33e97bf.svg"
+  },
+  {
+    "revision": "a3e2fda445d71f77c765",
+    "url": "/my-portfolio-v2/static/js/runtime~main.a3e2fda4.js"
+  },
+  {
+    "revision": "8eddf9d608b53e963dba",
+    "url": "/my-portfolio-v2/static/js/main.8eddf9d6.chunk.js"
+  },
+  {
+    "revision": "65454aa5107543526f58",
+    "url": "/my-portfolio-v2/static/js/1.65454aa5.chunk.js"
+  },
+  {
+    "revision": "8eddf9d608b53e963dba",
+    "url": "/my-portfolio-v2/static/css/main.fdd03b79.chunk.css"
+  },
+  {
+    "revision": "a71b016283de679ebb6cdb795685a6b2",
+    "url": "/my-portfolio-v2/index.html"
+  }
+];
